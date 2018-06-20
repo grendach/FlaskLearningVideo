54474dd94d664783fa020f460f450f7a7223732a
grendach <dmytro.grendach@nokia.com>
2018-06-20 14:15:51 +0200

    Flask video training #4

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This tutorial is based on YouTube chanel created by "thenewboston": https://www.youtube.com/watch?v=ZVGwqnjOKjk&t=2s
 
-I've just watching, playing with it, but mainly doing copy/paste development/1
+I've just watching, playing with it, but mainly doing copy/paste development    
--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-from flask import Flask, request
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "<h2>Hello, im FlaskLearing tutorial<h2>"
+@app.route("/profile/<name>")
+def profile(name):
+    return render_template("profile.html", name=name)
 
 
-@app.route("/method", methods = ['GET', 'POST'])
-def method():
-    if request.method == "POST":
-        return f"this is a {request.method} method"
-    else:
-        return f"this is a {request.method} method"
-
+# @app.route("/method", methods = ['GET', 'POST'])
+# def method():
+#     if request.method == "POST":
+#         return f"this is a {request.method} method"
+#     else:
+#         return f"this is a {request.method} method"
+#
 
 if __name__ == "__main__":
     app.run(debug=True)
+
