9c827539f80afdf4150241f2b469157be64dfe97
grendach <dmytro.grendach@nokia.com>
2018-06-21 13:24:54 +0200

    Video #7, Passing Objects itno tmplts

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def profile(name):
 def index(user=None):
     return render_template("user.html", user=user)
 
+@app.route("/shopping")
+def shopping():
+    food = ["Borsch", "Spagetti", "Salo", "Tuna"]
+    return render_template("shopping.html", food=food)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
