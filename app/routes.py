577a8ec101ccf19534a769b3382a2bc9bd6a231b
grendach <dmytro.grendach@nokia.com>
2018-07-02 08:55:30 +0200

    add 'for-loop' functionality

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,15 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Dmytro'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Wroclaw'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'Go away, you fucken assholes!'
+        }
+    ]
+    return render_template('index.html', title='Home',  user=user, posts=posts)
 
