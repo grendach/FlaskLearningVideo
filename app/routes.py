cacffd9d63e0db7292f09762c619ec3d1a61f645
grendach <dmytro.grendach@nokia.com>
2018-08-31 17:56:26 +0200

    create db

--- app/routes.py
+++ app/routes.py
@@ -9,12 +9,12 @@ def index():
     user = {'username': 'Dmytro'}
     posts = [
         {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'author': user,
+            'body': 'Linux',
         },
         {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
+            'author': {'username': 'Unknown User'},
+            'body': "Let's do some code",
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
