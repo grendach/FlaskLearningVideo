c628aa4f6722b84e9438782d87468673effcae2f
grendach <dmytro.grendach@nokia.com>
2018-09-09 11:24:38 +0200

    add 'last seen on' functionality

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 
+from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
@@ -6,6 +7,12 @@ from werkzeug.urls import url_parse
 from app import app, db
 from app.models import User
 from app.forms import LoginForm, RegistrationForm
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
