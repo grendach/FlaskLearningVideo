aa81f7727c3982157882d22f0c95e1ac321f4009
grendach <dmytro.grendach@nokia.com>
2018-09-12 12:28:19 +0200

    add route for following

--- app/routes.py
+++ app/routes.py
@@ -43,6 +43,37 @@ def user(username):
     return render_template('user.html', user=user, posts=posts)
 
 
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash(f'User {user} not found')
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot follow yourself')
+        return redirect(url_for('user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash(f'Now you are following {username}!')
+    return redirect(url_for('user', username=username))
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash(f'User {user} not found')
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot unfollow yourself')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash(f'Now you are NOT following {username}!')
+    return redirect (url_for('user', username=username))
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
