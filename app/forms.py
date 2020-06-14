3ed70433aabd1e0f2eb3d9104919b1fbf07937e8
Dmytro Grendach <grendach@gmail.com>
2020-06-14 22:17:15 +0300

    Chapter 5: User Logins

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username: ', validators = [DataRequired()])
     password = PasswordField('Pasword: ', validators = [DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign in')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username: ', validators = [DataRequired()])
+    password = PasswordField('Pasword: ', validators = [DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')]
+    )
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError(f'User <{username.data}> already exist. Use different name')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError(f'User with email <{email.data}> already exist. Use different email address')
